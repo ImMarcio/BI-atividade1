265c61e073b52157c1b4e94ebd491f9401868612
Marcio <marciojsilva159@gmail.com>
2024-10-29 10:08:58 -0300

    feat: resposta da 1 e 2 questão

--- main.py
+++ main.py
@@ -6,5 +6,20 @@ transportadoras = pd.read_csv('csvs/Transportadoras.csv')
 vendas_globais = pd.read_csv('csvs/Vendas Globais.csv')
 vendedores = pd.read_csv('csvs/Vendedores.csv')
 
-print(fornecedores.head())
-print(vendedores.head())
+
+
+
+#Questões
+
+#1. Quem são os meus 10 maiores clientes, em termos de vendas ($)?
+
+top_clientes = vendas_globais.groupby('ClienteNome')['Vendas'].sum().nlargest(10)
+print("Top 10 clientes: " )
+print(top_clientes)
+
+
+
+# 2. Quais os três maiores países, em termos de vendas ($)?
+print("Três maiores países " )
+top_paises = vendas_globais.groupby('ClientePaís')['Vendas'].sum().nlargest(3)
+print(top_paises)
